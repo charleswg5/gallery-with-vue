6730b654d0200974b5123f107c4f87e516e7fe65
charleswang <maoxc.wang@qq.com>
2017-03-25 11:14:44 +0800

    init commit

---
+++ src/components/Card.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+figure.card(@click="click")
+    img(src="src")
+    figcation
+        h2.card-title {{title}}
+        .card-back
+            p {{desc}}
+</template>
+
+<script>
+export default {
+    props:['id','src','title','desc'],
+    data:function(){
+        return {
+            'isBackface':false
+        }
+    },
+    methods:{
+
+    }
+}
+</script>
+
+<style lang="sass">
+
+</style>
